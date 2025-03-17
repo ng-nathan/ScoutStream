a095475a62576866cf9b15c7a3686182119756ba
Nước Mắm Code <77079010+nateng98@users.noreply.github.com>
2025-03-17 13:25:59 -0500

    Initial Commit

---
+++ ScoutStream/ScoutStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutStreamApp.swift
+//  ScoutStream
+//
+//  Created by Nathan Nguyen on 2025-03-17.
+//
+
+import SwiftUI
+
+@main
+struct ScoutStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
